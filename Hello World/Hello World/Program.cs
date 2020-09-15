643285e3fcdc7c08fbb763d327076d27be9407a3
ttf0512 <1799441665@qq.com>
2020-09-15 15:57:40 +0800

    春雨！

--- Hello World/Hello World/Program.cs
+++ Hello World/Hello World/Program.cs
@@ -7,6 +7,9 @@ namespace Hello_World
         static void Main(string[] args)
         {
             Console.WriteLine("Hello .Net Core 大傻春!");
+            Console.WriteLine("Hello .Net Core 大傻春!");
+            Console.WriteLine("Hello .Net Core 大傻春!");
+            Console.WriteLine("Hello .Net Core 大傻春!");
         }
     }
 }
